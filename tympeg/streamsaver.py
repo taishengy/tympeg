ecad24b9040182a35b2cb0f11ecca5b753db0e9c
TaiSheng Yeager <taishengyeager@gmail.com>
2017-08-03 18:21:23 -0700

    Adds analyzeduration and probesize attributes to StreamSaver

--- tympeg/streamsaver.py
+++ tympeg/streamsaver.py
@@ -7,6 +7,8 @@ from tympeg.util import renameFile
 class StreamSaver:
     def __init__(self, input_stream, output_file_path_ts, verbosity=24):
         self.file_writer = None
+        self.analyzeduration = 5000000  # ffmpeg default value (milliseconds must be integer)
+        self.probesize = 5000000  # ffmpeg default value (bytes must be > 32 and integer)
         directory, file_name = path.split(output_file_path_ts)
 
         # make sure output is .ts file for stable writing
@@ -19,7 +21,8 @@ class StreamSaver:
             file_name = renameFile(file_name)
             output_file_path_ts = path.join(directory, file_name)
 
-        self.args = ['ffmpeg', '-v', str(verbosity), '-i', str(input_stream), '-c', 'copy', output_file_path_ts]
+        self.args = ['ffmpeg', '-v', str(verbosity), '-analyzeduration', str(self.analyzeduration),
+                     '-probesize', str(self.probesize), '-i', str(input_stream), '-c', 'copy', output_file_path_ts]
 
     def run(self):
         self.file_writer = subprocess.run(self.args)
