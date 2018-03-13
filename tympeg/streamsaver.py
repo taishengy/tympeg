35d273b344ca028fdf60ff7e5431a3df3cb9359c
TaiSheng Yeager <taishengyeager@gmail.com>
2018-03-13 15:24:33 -0700

    Added documentation to streamsaver.py

--- tympeg/streamsaver.py
+++ tympeg/streamsaver.py
@@ -5,7 +5,16 @@ from tympeg.util import renameFile
 
 
 class StreamSaver:
+    """
+    Class that is responsible for writing network streams to disk. StreamSaver.run() must be called after initialization
+    """
     def __init__(self, input_stream, output_file_path_ts, verbosity=24):
+        """
+
+        :param input_stream: String, generally a url to a .m3u8 playlist
+        :param output_file_path_ts: String, output file. Any extension specified here will be rewritten to .ts. Avoid '.' in file names.
+        :param verbosity: int, FFMPEG verbosity level. Defaults fairly high to keep warning spam out of stdout.
+        """
         self.file_writer = None
         self.analyzeduration = 5000000  # ffmpeg default value (milliseconds must be integer)
         self.probesize = 5000000  # ffmpeg default value (bytes must be > 32 and integer)
@@ -25,7 +34,16 @@ class StreamSaver:
                      '-probesize', str(self.probesize), '-i', str(input_stream), '-c', 'copy', output_file_path_ts]
 
     def run(self):
+        """
+        Calls ffmpeg with StreamSaver arguments. Begins writing stream to disk.
+        :return:
+        """
         self.file_writer = subprocess.Popen(self.args)
 
     def quit(self):
+        """
+        Terminates StreamSaver.
+        :return:
+        """
+
         self.file_writer.terminate()
